48c3f508b0e1096778ffa08061e41024979199df
Teller501 <ande777j@stud.kea.dk>
2023-09-08 10:44:58 +0200

    updated day-2.js to use random hex colors

--- day-2.js
+++ day-2.js
@@ -1,11 +1,11 @@
 const elements = document.querySelectorAll("div");
 
 for (let i = 0; i < elements.length; i++) {
-    elements[i].style.background = "blue";
+    elements[i].style.background =  '#' + Math.random().toString(16).substr(-6);
 }
 
 document.getElementById("btn-1").addEventListener("click", function () {
-    document.getElementById("div-1").style.background = "red";
-    document.getElementById("div-2").style.background = "green";
-    document.getElementById("div-3").style.background = "yellow";
+    document.getElementById("div-1").style.background =  '#' + Math.random().toString(16).substr(-6);
+    document.getElementById("div-2").style.background =  '#' + Math.random().toString(16).substr(-6);
+    document.getElementById("div-3").style.background =  '#' + Math.random().toString(16).substr(-6);
 });
